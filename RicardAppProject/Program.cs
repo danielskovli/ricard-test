f2b75f0a52bb34a0aa999317e6c5fda3c44ccb83
Daniel Skovli <daniel.skovli@gmail.com>
2020-12-07 11:22:28 +1000

    Much better

--- RicardAppProject/Program.cs
+++ RicardAppProject/Program.cs
@@ -5,13 +5,12 @@ namespace RicardAppProject {
     class Program {
 
         static void Main(string[] args) {
-
-
             GradeBook book = new GradeBook();
             book.AddGrade(new Grade { User = "Ricard", Value = 91 });
             book.AddGrade(new Grade { User = "Ricard", Value = 85 });
             book.AddGrade(89.5f);
             book.AddGrade(87f);
+            book.AddGrade(55f);
 
             var stats = new GradeStatistics(book);
             stats.PrintStats();
